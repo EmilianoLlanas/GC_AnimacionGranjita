a5cbd07fc437de62779618504cb59701aefe8b43
Luis Angel Diaz Rebollo <luisangel.diaz188@gmail.com>
2022-03-23 14:10:27 -0600

    Initial Commit

--- cottage.cpp
+++ cottage.cpp
@@ -177,254 +177,242 @@ private:
     void createWall()
     {
         GLdouble (*p)[3][POINTS] = &renderMatrix;
+        GLfloat colors [3] = {0.612, 0.365, 0.306};
 
-        glColor3f(0.612, 0.365, 0.306);
-        glLineWidth(3);
+        GLint width = 3;
+        int x1, x2, y1, y2;
 
         //Horizontal
-        glBegin(GL_LINES);
-            glVertex2d((*p)[0][0],(*p)[1][0]);
-            glVertex2d((*p)[0][5],(*p)[1][5]);
-        glEnd();
-        glBegin(GL_LINES);
-            glVertex2d((*p)[0][6],(*p)[1][6]);
-            glVertex2d((*p)[0][11],(*p)[1][11]);
-        glEnd();
+        x1 = (*p)[0][0]; y1 = (*p)[1][0];
+        x2 = (*p)[0][5]; y2 = (*p)[1][5];
+        linea.draw(x1, y1, x2, y2, colors, width);
+
+        x1 = (*p)[0][6]; y1 = (*p)[1][6];
+        x2 = (*p)[0][11]; y2 = (*p)[1][11];
+        linea.draw(x1, y1, x2, y2, colors, width);
 
         //Vertical
-        glBegin(GL_LINES);
-            glVertex2d((*p)[0][0],(*p)[1][0]);
-            glVertex2d((*p)[0][6],(*p)[1][6]);
-        glEnd();
-        glBegin(GL_LINES);
-            glVertex2d((*p)[0][1],(*p)[1][1]);
-            glVertex2d((*p)[0][7],(*p)[1][7]);
-        glEnd();
-        glBegin(GL_LINES);
-            glVertex2d((*p)[0][1],(*p)[1][2]);
-            glVertex2d((*p)[0][8],(*p)[1][8]);
-        glEnd();
-        glBegin(GL_LINES);
-            glVertex2d((*p)[0][3],(*p)[1][3]);
-            glVertex2d((*p)[0][9],(*p)[1][9]);
-        glEnd();
-        glBegin(GL_LINES);
-            glVertex2d((*p)[0][4],(*p)[1][4]);
-            glVertex2d((*p)[0][10],(*p)[1][10]);
-        glEnd();
-        glBegin(GL_LINES);
-            glVertex2d((*p)[0][5],(*p)[1][5]);
-            glVertex2d((*p)[0][11],(*p)[1][11]);
-        glEnd();
+        x1 = (*p)[0][0]; y1 = (*p)[1][0];
+        x2 = (*p)[0][6]; y2 = (*p)[1][6];
+        linea.draw(x1, y1, x2, y2, colors, width);
+
+        x1 = (*p)[0][1]; y1 = (*p)[1][1];
+        x2 = (*p)[0][7]; y2 = (*p)[1][7];
+        linea.draw(x1, y1, x2, y2, colors, width);
+
+        x1 = (*p)[0][1]; y1 = (*p)[1][2];
+        x2 = (*p)[0][8]; y2 = (*p)[1][8];
+        linea.draw(x1, y1, x2, y2, colors, width);
+
+        x1 = (*p)[0][3]; y1 = (*p)[1][3];
+        x2 = (*p)[0][9]; y2 = (*p)[1][9];
+        linea.draw(x1, y1, x2, y2, colors, width);
+
+        x1 = (*p)[0][4]; y1 = (*p)[1][4];
+        x2 = (*p)[0][10]; y2 = (*p)[1][10];
+        linea.draw(x1, y1, x2, y2, colors, width);
+
+        x1 = (*p)[0][5]; y1 = (*p)[1][5];
+        x2 = (*p)[0][11]; y2 = (*p)[1][11];
+        linea.draw(x1, y1, x2, y2, colors, width);
     }
 
     void createWindow()
     {
         GLdouble (*p)[3][POINTS] = &renderMatrix;
 
-        GLfloat colors[3] = {0.941,0.808,0.620};
+        GLfloat colors[3] = {0.941, 0.808, 0.620};
         GLfloat width = 3;
         int x1, y1, x2, y2;
 
         //Middle
-            x1 = (*p)[0][16];
-            y1 = (*p)[1][16];
-            x2 = (*p)[0][17];
-            y2 = (*p)[1][17];
-            linea.draw(x1,y1,x2,y2, colors, width);
+        x1 = (*p)[0][16]; y1 = (*p)[1][16];
+        x2 = (*p)[0][17]; y2 = (*p)[1][17];
+        linea.draw(x1,y1,x2,y2, colors, width);
 
-            x1 = (*p)[0][18];
-            y1 = (*p)[1][18];
-            x2 = (*p)[0][19];
-            y2 = (*p)[1][19];
-            linea.draw(x1,y1,x2,y2, colors, width);
+        x1 = (*p)[0][18]; y1 = (*p)[1][18];
+        x2 = (*p)[0][19]; y2 = (*p)[1][19];
+        linea.draw(x1,y1,x2,y2, colors, width);
 
-        glColor3f(0.267, 0.184, 0.255);
+        colors[0] = 0.267;
+        colors[1] = 0.184;
+        colors[2] = 0.255;
 
         //Outline
-        glBegin(GL_LINES);
-            glVertex2d((*p)[0][12],(*p)[1][12]);
-            glVertex2d((*p)[0][13],(*p)[1][13]);
-        glEnd();
-        glBegin(GL_LINES);
-            glVertex2d((*p)[0][14],(*p)[1][14]);
-            glVertex2d((*p)[0][15],(*p)[1][15]);
-        glEnd();
-        glBegin(GL_LINES);
-            glVertex2d((*p)[0][12],(*p)[1][12]);
-            glVertex2d((*p)[0][14],(*p)[1][14]);
-        glEnd();
-        glBegin(GL_LINES);
-            glVertex2d((*p)[0][13],(*p)[1][13]);
-            glVertex2d((*p)[0][15],(*p)[1][15]);
-        glEnd();
+        x1 = (*p)[0][12]; y1 = (*p)[1][12];
+        x2 = (*p)[0][13]; y2 = (*p)[1][13];
+        linea.draw(x1,y1,x2,y2, colors, width);
+
+        x1 = (*p)[0][14]; y1 = (*p)[1][14];
+        x2 = (*p)[0][15]; y2 = (*p)[1][15];
+        linea.draw(x1,y1,x2,y2, colors, width);
+
+        x1 = (*p)[0][12]; y1 = (*p)[1][12];
+        x2 = (*p)[0][14]; y2 = (*p)[1][14];
+        linea.draw(x1,y1,x2,y2, colors, width);
+
+        x1 = (*p)[0][13]; y1 = (*p)[1][13];
+        x2 = (*p)[0][15]; y2 = (*p)[1][15];
+        linea.draw(x1,y1,x2,y2, colors, width);
     }
 
     void createStairs()
     {
         GLdouble (*p)[3][POINTS]= &renderMatrix;
 
-        glColor3f(0.345, 0.220, 0.263);
-        glLineWidth(3);
+        GLfloat colors[3] = {0.345, 0.220, 0.263};
+        GLfloat width = 3;
+        int x1, y1, x2, y2;
 
         //First Level
-        glBegin(GL_LINES);
-            glVertex2d((*p)[0][22],(*p)[1][22]);
-            glVertex2d((*p)[0][28],(*p)[1][28]);
-        glEnd();
-        glBegin(GL_LINES);
-            glVertex2d((*p)[0][23],(*p)[1][23]);
-            glVertex2d((*p)[0][29],(*p)[1][29]);
-        glEnd();
-        glBegin(GL_LINES);
-            glVertex2d((*p)[0][25],(*p)[1][25]);
-            glVertex2d((*p)[0][26],(*p)[1][26]);
-        glEnd();
-        glBegin(GL_LINES);
-            glVertex2d((*p)[0][28],(*p)[1][28]);
-            glVertex2d((*p)[0][29],(*p)[1][29]);
-        glEnd();
+        x1 = (*p)[0][22]; y1 = (*p)[1][22];
+        x2 = (*p)[0][28]; y2 = (*p)[1][28];
+        linea.draw(x1,y1,x2,y2, colors, width);
+
+        x1 = (*p)[0][23]; y1 = (*p)[1][23];
+        x2 = (*p)[0][29]; y2 = (*p)[1][29];
+        linea.draw(x1,y1,x2,y2, colors, width);
+
+        x1 = (*p)[0][25]; y1 = (*p)[1][25];
+        x2 = (*p)[0][26]; y2 = (*p)[1][26];
+        linea.draw(x1,y1,x2,y2, colors, width);
+
+        x1 = (*p)[0][28]; y1 = (*p)[1][28];
+        x2 = (*p)[0][29]; y2 = (*p)[1][29];
+        linea.draw(x1,y1,x2,y2, colors, width);
 
         //Second Level
-        glBegin(GL_LINES);
-            glVertex2d((*p)[0][24],(*p)[1][24]);
-            glVertex2d((*p)[0][27],(*p)[1][27]);
-        glEnd();
-        glBegin(GL_LINES);
-            glVertex2d((*p)[0][26],(*p)[1][26]);
-            glVertex2d((*p)[0][27],(*p)[1][27]);
-        glEnd();
+        x1 = (*p)[0][24]; y1 = (*p)[1][24];
+        x2 = (*p)[0][27]; y2 = (*p)[1][27];
+        linea.draw(x1,y1,x2,y2, colors, width);
+
+        x1 = (*p)[0][26]; y1 = (*p)[1][26];
+        x2 = (*p)[0][27]; y2 = (*p)[1][27];
+        linea.draw(x1,y1,x2,y2, colors, width);
     }
 
     void createStick()
     {
         GLdouble (*p)[3][POINTS] = &renderMatrix;
+        GLfloat colors[3] = {0.941, 0.808, 0.620};
 
-        glColor3f(0.941, 0.808, 0.620);
-        glLineWidth(3);
+        GLfloat width = 3;
+        int x1, y1, x2, y2;
 
         //Middle
-        glBegin(GL_LINES);
-            glVertex2d((*p)[0][30],(*p)[1][30]);
-            glVertex2d((*p)[0][31],(*p)[1][31]);
-        glEnd();
-        glBegin(GL_LINES);
-            glVertex2d((*p)[0][30],(*p)[1][30]);
-            glVertex2d((*p)[0][32],(*p)[1][32]);
-        glEnd();
-        glBegin(GL_LINES);
-            glVertex2d((*p)[0][32],(*p)[1][32]);
-            glVertex2d((*p)[0][33],(*p)[1][33]);
-        glEnd();
+        x1 = (*p)[0][30]; y1 = (*p)[1][30];
+        x2 = (*p)[0][31]; y2 = (*p)[1][31];
+        linea.draw(x1,y1,x2,y2, colors, width);
+
+        x1 = (*p)[0][30]; y1 = (*p)[1][30];
+        x2 = (*p)[0][32]; y2 = (*p)[1][32];
+        linea.draw(x1,y1,x2,y2, colors, width);
+
+        x1 = (*p)[0][32]; y1 = (*p)[1][32];
+        x2 = (*p)[0][33]; y2 = (*p)[1][33];
+        linea.draw(x1,y1,x2,y2, colors, width);
     }
 
     void createRoof()
     {
         GLdouble (*p)[3][POINTS] = &renderMatrix;
-        int i;
+        GLfloat colors[3] = {0.271, 0.176, 0.231};
 
-        glColor3f(0.271, 0.176, 0.231);
-        glLineWidth(3);
+        GLfloat width = 3;
+        int x1, y1, x2, y2, i;
 
         //First Level
             //Horizontal
-            glBegin(GL_LINES);
-                glVertex2d((*p)[0][34],(*p)[1][34]);
-                glVertex2d((*p)[0][44],(*p)[1][44]);
-            glEnd();
-            glBegin(GL_LINES);
-                glVertex2d((*p)[0][45],(*p)[1][45]);
-                glVertex2d((*p)[0][55],(*p)[1][55]);
-            glEnd();
+            x1 = (*p)[0][34]; y1 = (*p)[1][34];
+            x2 = (*p)[0][44]; y2 = (*p)[1][44];
+            linea.draw(x1,y1,x2,y2, colors, width);
+
+            x1 = (*p)[0][45]; y1 = (*p)[1][45];
+            x2 = (*p)[0][55]; y2 = (*p)[1][55];
+            linea.draw(x1,y1,x2,y2, colors, width);
 
             //Vertical
             for (i = 0; i < 11; i++) {
-                glBegin(GL_LINES);
-                    glVertex2d((*p)[0][34 + i],(*p)[1][34 + i]);
-                    glVertex2d((*p)[0][45 + i],(*p)[1][45 + i]);
-                glEnd();
+                x1 = (*p)[0][34 + i]; y1 = (*p)[1][34 + i];
+                x2 = (*p)[0][45 + i]; y2 = (*p)[1][45 + i];
+                linea.draw(x1,y1,x2,y2, colors, width);
             }
 
         //Second Level
             //Horizontal
-            glBegin(GL_LINES);
-                glVertex2d((*p)[0][56],(*p)[1][56]);
-                glVertex2d((*p)[0][65],(*p)[1][65]);
-            glEnd();
-            glBegin(GL_LINES);
-                glVertex2d((*p)[0][66],(*p)[1][66]);
-                glVertex2d((*p)[0][75],(*p)[1][75]);
-            glEnd();
+            x1 = (*p)[0][56]; y1 = (*p)[1][56];
+            x2 = (*p)[0][65]; y2 = (*p)[1][65];
+            linea.draw(x1,y1,x2,y2, colors, width);
+
+            x1 = (*p)[0][66]; y1 = (*p)[1][66];
+            x2 = (*p)[0][75]; y2 = (*p)[1][75];
+            linea.draw(x1,y1,x2,y2, colors, width);
 
             //Vertical
             for (i = 0; i < 10; i++) {
-                glBegin(GL_LINES);
-                    glVertex2d((*p)[0][56 + i],(*p)[1][56 + i]);
-                    glVertex2d((*p)[0][66 + i],(*p)[1][66 + i]);
-                glEnd();
+                x1 = (*p)[0][56 + i]; y1 = (*p)[1][56 + i];
+                x2 = (*p)[0][66 + i]; y2 = (*p)[1][66 + i];
+                linea.draw(x1,y1,x2,y2, colors, width);
             }
 
         //Third Level
             //Horizontal
-            glBegin(GL_LINES);
-                glVertex2d((*p)[0][76],(*p)[1][76]);
-                glVertex2d((*p)[0][83],(*p)[1][83]);
-            glEnd();
-            glBegin(GL_LINES);
-                glVertex2d((*p)[0][84],(*p)[1][84]);
-                glVertex2d((*p)[0][91],(*p)[1][91]);
-            glEnd();
+            x1 = (*p)[0][76]; y1 = (*p)[1][76];
+            x2 = (*p)[0][83]; y2 = (*p)[1][83];
+            linea.draw(x1,y1,x2,y2, colors, width);
+
+            x1 = (*p)[0][84]; y1 = (*p)[1][84];
+            x2 = (*p)[0][91]; y2 = (*p)[1][91];
+            linea.draw(x1,y1,x2,y2, colors, width);
 
             //Vertical
             for (i = 0; i < 8; i++) {
-                glBegin(GL_LINES);
-                    glVertex2d((*p)[0][76 + i],(*p)[1][76 + i]);
-                    glVertex2d((*p)[0][84 + i],(*p)[1][84 + i]);
-                glEnd();
+                x1 = (*p)[0][76 + i]; y1 = (*p)[1][76 + i];
+                x2 = (*p)[0][84 + i]; y2 = (*p)[1][84 + i];
+                linea.draw(x1,y1,x2,y2, colors, width);
             }
 
         //Fouth Level
             //Horizontal
-            glBegin(GL_LINES);
-                glVertex2d((*p)[0][92],(*p)[1][92]);
-                glVertex2d((*p)[0][98],(*p)[1][98]);
-            glEnd();
-            glBegin(GL_LINES);
-                glVertex2d((*p)[0][99],(*p)[1][99]);
-                glVertex2d((*p)[0][105],(*p)[1][105]);
-            glEnd();
+            x1 = (*p)[0][92]; y1 = (*p)[1][92];
+            x2 = (*p)[0][98]; y2 = (*p)[1][98];
+            linea.draw(x1,y1,x2,y2, colors, width);
+
+            x1 = (*p)[0][99]; y1 = (*p)[1][99];
+            x2 = (*p)[0][105]; y2 = (*p)[1][105];
+            linea.draw(x1,y1,x2,y2, colors, width);
 
             //Vertical
             for (i = 0; i < 7; i++) {
-                glBegin(GL_LINES);
-                    glVertex2d((*p)[0][92 + i],(*p)[1][92 + i]);
-                    glVertex2d((*p)[0][99 + i],(*p)[1][99 + i]);
-                glEnd();
+                x1 = (*p)[0][92 + i]; y1 = (*p)[1][92 + i];
+                x2 = (*p)[0][99 + i]; y2 = (*p)[1][99 + i];
+                linea.draw(x1,y1,x2,y2, colors, width);
             }
 
         //Fifth Level
             //Horizontal
-            glBegin(GL_LINES);
-                glVertex2d((*p)[0][106],(*p)[1][106]);
-                glVertex2d((*p)[0][110],(*p)[1][110]);
-            glEnd();
-            glBegin(GL_LINES);
-                glVertex2d((*p)[0][111],(*p)[1][111]);
-                glVertex2d((*p)[0][115],(*p)[1][115]);
-            glEnd();
+            x1 = (*p)[0][106]; y1 = (*p)[1][106];
+            x2 = (*p)[0][110]; y2 = (*p)[1][110];
+            linea.draw(x1,y1,x2,y2, colors, width);
+
+            x1 = (*p)[0][111]; y1 = (*p)[1][111];
+            x2 = (*p)[0][115]; y2 = (*p)[1][115];
+            linea.draw(x1,y1,x2,y2, colors, width);
 
             //Vertical
             for (i = 0; i < 5; i++) {
-                glBegin(GL_LINES);
-                    glVertex2d((*p)[0][106 + i],(*p)[1][106 + i]);
-                    glVertex2d((*p)[0][111 + i],(*p)[1][111 + i]);
-                glEnd();
+                x1 = (*p)[0][106 + i]; y1 = (*p)[1][106 + i];
+                x2 = (*p)[0][111 + i]; y2 = (*p)[1][111 + i];
+                linea.draw(x1,y1,x2,y2, colors, width);
             }
     }
 
 public:
     void draw()
     {
+        //Load points in renderMatrix
+        matrix.multiplication(opMatrix, globalPoints, renderMatrix);
+
         createWall();
         createWindow();
         createStairs();
@@ -439,8 +427,5 @@ public:
         //Define operation matrix
         matrix.scaling(15,15,opMatrix);
         matrix.translation(-40,-15,opMatrix);
-
-        //Load points in renderMatrix
-        matrix.multiplication(opMatrix, globalPoints, renderMatrix);
     }
 };
